58e44aa032956f0fb335a9b5a8fc2a01ef29ad9f
Antoni Bertran <antoni@tresipunt.com>
2014-04-14 10:15:39 +0200

    To allow nagios monitorization

---
+++ src/check_environment.php
@@ -0,0 +1,9 @@
+<?php
+require_once dirname(__FILE__).'/classes/lang.php';
+require_once dirname(__FILE__).'/classes/constants.php';
+require_once dirname(__FILE__).'/classes/gestorBD.php';
+
+	
+$gestorBD	= new GestorBD();
+?>
+OK
