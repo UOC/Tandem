ef2820ed572bc54074ae5acaca186db6385751e9
Antoni Bertran <antoni@tresipunt.com>
2014-10-14 08:30:29 +0200

    Open video chat if it set

--- src/classes/utils.php
+++ src/classes/utils.php
@@ -327,3 +327,18 @@ function convertToUtf8 ($s) {
     }
     return $s;
 }
+/**
+ * Get the current url
+ * @return [type] [description]
+ */
+function curPageURL() {
+ $pageURL = 'http';
+ if (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+ $pageURL .= "://";
+ if ($_SERVER["SERVER_PORT"] != "80") {
+  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+ } else {
+  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+ }
+ return $pageURL;
+}
--- src/waiting4user.php
+++ src/waiting4user.php
@@ -1,5 +1,8 @@
 <?php
 require_once dirname(__FILE__).'/classes/lang.php';
+require_once dirname(__FILE__).'/classes/constants.php';
+require_once dirname(__FILE__).'/classes/utils.php';
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -7,7 +10,10 @@ require_once dirname(__FILE__).'/classes/lang.php';
 <title>Tandem Pantalla Inici_ParaExportar</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <script>
+<?php 
+if ($_SESSION[OPEN_TOOL_ID]>0){?>
 	top.setExpiredNow(60);
+<?php } ?>
 </script>
 <style type="text/css">
 <!--
@@ -30,10 +36,18 @@ require_once dirname(__FILE__).'/classes/lang.php';
 <p style="text-align:center;"><?php echo $LanguageInstance->get('acceptance');?><br/>
 <h4 style="text-align:center;"><span id="startNowBtn"></span></h4></p>
 <?php 
-if ($_SESSION[OPEN_TOOL_ID]>0){?>
+if ($_SESSION[OPEN_TOOL_ID]>0){
+$url_2_return = str_replace("waiting4user.php","sampleConfirm.php",curPageURL());
+$id_register_tandem = isset($_SESSION[CURRENT_TANDEM])?$_SESSION[CURRENT_TANDEM]:false;
+$separtor = '?';
+if (strpos($url_2_return, '?')!==false){
+	$separtor = '&';
+}
+$url_2_return .= $separtor.'id='.$id_register_tandem;
+	?>
 <script language="javascript">
 window.onbeforeunload = function() {};
-document.location.href='ltiConsumer.php?id=<?php echo $_SESSION[OPEN_TOOL_ID]?>';
+document.location.href='ltiConsumer.php?id=<?php echo $_SESSION[OPEN_TOOL_ID]?>&return_url=<?php echo $url_2_return?>';
 </script>
 <?php }?>
 </body>
