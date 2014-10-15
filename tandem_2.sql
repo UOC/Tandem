8fbf2d5650c4a1a4eeccba8f2c6660c746f787d3
Antoni Bertran <antoni@tresipunt.com>
2014-10-16 00:10:21 +0200

    Created new tables to manage tandems to with external tools

--- tandem_2.sql
+++ tandem_2.sql
@@ -1,28 +1,3 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Versión 4096
-#
-# http://www.sequelpro.com/
-# http://code.google.com/p/sequel-pro/
-#
-# Host: 127.0.0.1 (MySQL 5.5.9)
-# Base de datos: tandem
-# Tiempo de Generación: 2014-09-01 10:03:34 +0000
-# ************************************************************
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
-# Volcado de tabla course
-# ------------------------------------------------------------
-
 DROP TABLE IF EXISTS `course`;
 
 CREATE TABLE `course` (
@@ -1499,10 +1474,20 @@ CREATE TABLE `waiting_room_user_history` (
 
 
 
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+create table mooc_tandem
+(
+id bigint auto_increment,
+id_tandem int (11),
+id_videochat int (11),
+id_user int (11),
+language varchar(10),
+id_partner int (11),
+partner_language varchar(10),
+primary key (id)
+);
+create table mooc_tandem_form(
+id_mooc_tandem bigint,
+feedback_form longtext,
+rating_partner_feedback_form longtext,
+primary key (id_mooc_tandem) 
+);
