3b501b6fe71836329c997511a737785675b6a4c3
victor <victor@tresipunt.com>
2014-10-23 17:52:31 +0200

    ranking

---
+++ src/ranking.php
@@ -0,0 +1,72 @@
+<?php
+
+
+require_once dirname(__FILE__) . '/classes/lang.php';
+require_once dirname(__FILE__) . '/classes/constants.php';
+require_once dirname(__FILE__) . '/classes/gestorBD.php';
+require_once 'IMSBasicLTI/uoc-blti/lti_utils.php';
+
+
+
+$user_obj = isset($_SESSION[CURRENT_USER]) ? $_SESSION[CURRENT_USER] : false;
+$course_id = isset($_SESSION[COURSE_ID]) ? $_SESSION[COURSE_ID] : false;
+if (!$user_obj) {
+//Tornem a l'index
+	header('Location: index.php');
+} else {	
+	$gestorBD = new GestorBD();  	
+	$usersRanking = $gestorBD->getUsersRanking($course_id);	
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container" style='margin-top:20px'>
+	<div class='row'>
+		<div class='col-md-12'>
+			<button class="btn btn-success" type='button' onclick="window.location ='portfolio.php';">
+				<?php echo $LanguageInstance->get('Go to your portfolio') ?></button>
+		</div>
+	</div>
+  <div class="row">
+  	<div class='col-md-6'>
+  	<h2 class='title'><?php echo $LanguageInstance->get('Users ranking');?></h2>
+  	</div>
+  	<div class='col-md-6'>
+  	<p class='text-right'>
+		<a href="#" title="<?php echo $LanguageInstance->get('tandem_logo')?>"><img src="css/images/logo_Tandem.png" alt="<?php echo $LanguageInstance->get('tandem_logo')?>" /></a>					
+  	</p>
+  	</div>
+  <div class="col-md-12">
+  	<table class="table table-striped">
+  	<tr>
+	  	<th><?php echo $LanguageInstance->get('Num');?></th>
+	  	<th><?php echo $LanguageInstance->get('User');?></th>
+	  	<th><?php echo $LanguageInstance->get('Total time');?></th>
+  	</tr>
+ 	<?php
+	  if(!empty($usersRanking)){
+	  	$cont = 1;
+	  	foreach($usersRanking as $f){
+	  	echo "<tr>";
+	  	echo "<td>".$cont."</td>";
+	  	echo "<td>".$f['user']."</td>
+	  		  <td>".$f['total_time']."</td>
+	  		  </tr>";	
+	  	$cont++;
+	  	}
+	  }
+  	?>
+  </table>
+  </div>
+  </div>
+</div>
+</body>
+</html>
+
+
