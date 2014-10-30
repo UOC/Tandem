18697fe29b85ad60dc129273018f6737f17cd1b7
Antoni Bertran <antoni@tresipunt.com>
2014-10-30 11:53:48 +0100

    End session

--- src/api/endSession.php
+++ src/api/endSession.php
@@ -6,6 +6,7 @@ require_once dirname(__FILE__).'/../classes/gestorBD.php';
 require_once dirname(__FILE__).'/../classes/IntegrationTandemBLTI.php';
 
 $id = isset($_REQUEST['id'])?intval($_REQUEST['id'],10):0;
+$user_id = isset($_REQUEST['user_id'])?intval($_REQUEST['user_id'],10):0;
 
 $tandem = false;
 $gestorBD = new GestorBD();
@@ -18,6 +19,18 @@ if ($id>0) {
 }
 
 if ($tandem) {	
+	$username = $gestorBD->getUserName($user_id);
+	$exercise = $tandem['name_xml_file'];
+	$room = sanitise_string($exercise.getTandemIdentifier($id, $tandem['id_resource_lti']));
+	$tandemBLTI = new IntegrationTandemBLTI();
+	//we need to identify the exercise
+	//Now we try to get data course	
+	$relative_path = isset($tandem['relative_path']) && strlen($tandem['relative_path'])>0 ? $tandem['relative_path'].DIRECTORY_SEPARATOR:'';
+	$data_exercise = $tandemBLTI->getDataExercise($exercise, true, $relative_path);
+
+	$room = $tandem['name_xml_file'].$tandem['id_resource_lti']."_".$tandem['id'];
+	$tandemBLTI->endSessionExternalToolXMLUser($user_id, $room,$username);
+	$return->result = 'ok';
 
 	
 }
