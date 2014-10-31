c7f7184452988973538d6326111312a5e98184cb
Antoni Bertran <antoni@tresipunt.com>
2014-10-31 09:04:45 +0100

    Update the session Available

--- src/api/sessionAvailable.php
+++ src/api/sessionAvailable.php
@@ -6,6 +6,7 @@ require_once dirname(__FILE__).'/../classes/gestorBD.php';
 require_once dirname(__FILE__).'/../classes/IntegrationTandemBLTI.php';
 
 $id = isset($_REQUEST['id'])?intval($_REQUEST['id'],10):0;
+$download_url = isset($_REQUEST['download_url'])?$_REQUEST['download_url']:'';
 
 $tandem = false;
 $gestorBD = new GestorBD();
@@ -20,6 +21,9 @@ if ($id>0) {
 if ($tandem) {	
 
 	$gestorBD->updateTandemSessionAvailable($id);
+	if (strlen($download_url)>0) {
+		$gestorBD->updateDownloadVideoUrlFeedbackTandemByTandemId($id, $download_url);
+	}
 	$return->result = 'ok';	
 
 	
