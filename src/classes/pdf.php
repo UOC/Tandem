fe0c81f57cff34255191395bd1926f482c264973
Antoni Bertran <antoni@tresipunt.com>
2014-11-29 11:38:33 +0100

    Changes on PDF to export list

--- src/classes/pdf.php
+++ src/classes/pdf.php
@@ -3,6 +3,51 @@ require_once ("tcpdf_min/tcpdf.php");
 require_once dirname(__FILE__) . '/lang.php';
 
 
+class TandemPDF extends TCPDF {
+
+	protected $LanguageInstance = false;
+	protected $username = '';
+	protected $image_file = false;
+
+	public function __construct($orientation='P', $unit='mm', $format='A4', $unicode=true, $encoding='UTF-8', $diskcache=false, $pdfa=false, $image_file=false, $LanguageInstance=false, $username='') {
+		parent::__construct($orientation, $unit, $format, $unicode, $encoding, $diskcache, $pdfa);
+		$this->LanguageInstance = $LanguageInstance;
+		$this->username = $username;
+		$this->image_file = $image_file;
+	}
+	
+
+    //Page header
+    public function Header() {
+        // Logo
+        /*$this->Image($this->image_file, 2, 0, 0, 0, 'PNG', '', 'T', false, 300, '', false, false, 0, false, false, false);
+        // Set font
+        $this->SetFont('helvetica', 'B', 20);
+        // Title
+        $this->Cell(0, 0, $this->LanguageInstance->get("List of all your tandems").' '.$this->username, 0, false, 'C', 0, '', 0, false, 'M', 'B');
+*/
+
+        $this->Image($this->image_file, 10, 10, 26, '', 'PNG', '', 'T', false, 300, '', false, false, 0, false, false, false);
+        // Set font
+        $this->SetFont('helvetica', 'B', 20);
+        // Title
+        $this->Cell(0, 26, $this->LanguageInstance->getTag("List of all %s tandems",$this->username), 0, false, 'C', 0, '', 0, false, 'M', 'M');        
+        $style = array('width' => 0.1, 'cap' => 'butt', 'join' => 'miter', 'dash' => 0, 'color' => array(0, 0, 0));
+		$this->Line(3, 22, 208, 22, $style);
+    }
+
+    // Page footer
+    public function Footer() {
+        // Position at 15 mm from bottom
+        $this->SetY(-15);
+        // Set font
+        $this->SetFont('helvetica', 'I', 8);
+        // Page number
+        $this->Cell(0, 10, $this->LanguageInstance->getTag("Generated on %s", date('Y-m-d')).'- Page '.$this->getAliasNumPage().'/'.$this->getAliasNbPages(), 0, false, 'C', 0, 'R', 0, false, 'T', 'M');
+    }
+}
+
+
 
 function generatePDF($user_id,$course_id){
 
@@ -16,17 +61,17 @@ print_r($feedBacks);
 echo "</pre>";
 die();*/
 // create new PDF document
-$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+// 					$orientation='P', $unit='mm', 	$format='A4', 	$unicode=true, $encoding='UTF-8', $diskcache=false, $pdfa=false, $image_file=false, $LanguageInstance=false, $username='') {
+$pdf = new TandemPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 		'UTF-8', 		 false, 			false, dirname(__FILE__).'/tcpdf_min/logo_Tandem.png', $LanguageInstance, $username);
 
 // set document information
 $pdf->SetCreator('UOC');
 $pdf->SetTitle('Tandem');
-$pdf->SetSubject('Tandem Portfolio');
-
+$pdf->SetSubject('Tandem Portfolio '.$username);
 
 // set default header data
-$pdf->SetHeaderData('logo_Tandem.png', PDF_HEADER_LOGO_WIDTH, '', '', array(0,64,255), array(87,87,89));
-$pdf->setFooterData(array(0,64,0), array(87,87,89));
+//$pdf->SetHeaderData('logo_Tandem.png', PDF_HEADER_LOGO_WIDTH, $LanguageInstance->get("List of all your tandems").' '.$username, '', array(0,64,255), array(87,87,89));
+//$pdf->setFooterData(array(0,64,0), array(87,87,89));
 
 // set header and footer fonts
 $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
@@ -79,12 +124,30 @@ $html = "
 	</style>
 ";
 $html .= "<body>";
-$html .= "<div><span>List of all your tandems</span></div>";
-
-
+$user_position_ranking = 0;
+$user_data = $gestorBD->getRankingUserData($user_id,$course_id);
+if ($user_data && isset($user_data['lang'])) {
+	$user_position_ranking = $gestorBD->getUserRankingPosition($user_id,$user_data['lang'],$course_id);
+}
+$show_tandem = false;
 if(!empty($feedBacks)){
+
 	foreach($feedBacks as $key => $value){
 		if(!empty($value['feedback_form'])){
+
+			if (!$show_tandem) {
+				$html .="<p>".$LanguageInstance->get('SUMMARY')."<div align=\"center\"><table width=\"50%\" border=\"1\">";
+				if ($user_position_ranking>0) {
+					$html  .= "<tr><td>".$LanguageInstance->get('Ranking Position').": <b>".$user_position_ranking."</b></td></tr>";
+				}
+				$html .="<tr><td>".$LanguageInstance->get('Fluency').": <b>".$user_data['fluency']."%</b></td></tr>
+						<tr><td>".$LanguageInstance->get('Accuracy').": <b>".$user_data['accuracy']."%</b></td></tr>
+						<tr><td>".$LanguageInstance->get('Overall Grade').": <b>".getSkillsLevel(getOverallAsIdentifier($user_data['overall_grade']),$LanguageInstance)."</b></td></tr>
+					</table></div>
+				</p>";						
+			}
+			$show_tandem = true;
+
 			$value['feedback_form'] = @unserialize($value['feedback_form']);
 			 $title = str_replace("%1",$value['created'],$LanguageInstance->get("Tandem session created %1 with a total duration of %2"));
 			 $title = str_replace("%2",$value['total_time'],$title);
@@ -93,7 +156,7 @@ if(!empty($feedBacks)){
 			$html .="<p><ul>
 							<li >".$LanguageInstance->get('Fluency').": <b>".$value['feedback_form']->fluency."%</b></li>
 							<li >".$LanguageInstance->get('Accuracy').": <b>".$value['feedback_form']->accuracy."%</b></li>
-							<li >".$LanguageInstance->get('Overall Grade').": <b>".$gestorBD->getSkillsLevel($value['feedback_form']->grade,$LanguageInstance)."</b></li>				
+							<li >".$LanguageInstance->get('Overall Grade').": <b>".getSkillsLevel($value['feedback_form']->grade,$LanguageInstance)."</b></li>				
 							<li >".$LanguageInstance->get('Pronunciation').": <b>".$value['feedback_form']->pronunciation."</b></li>
 							<li >".$LanguageInstance->get('Vocabulary').": <b>".$value['feedback_form']->vocabulary."</b></li>
 							<li >".$LanguageInstance->get('Grammar').": <b>".$value['feedback_form']->grammar."</b></li>
@@ -108,7 +171,7 @@ if(!empty($feedBacks)){
 					$html .="<p> <ul>
 									<li >".$LanguageInstance->get('Fluency').": <b>".$partnerFeedback->fluency."%</b></li>
 									<li >".$LanguageInstance->get('Accuracy').": <b>".$partnerFeedback->accuracy."%</b></li>
-									<li >".$LanguageInstance->get('Overall Grade').": <b>".$gestorBD->getSkillsLevel($partnerFeedback->grade,$LanguageInstance)."</b></li>				
+									<li >".$LanguageInstance->get('Overall Grade').": <b>".getSkillsLevel($partnerFeedback->grade,$LanguageInstance)."</b></li>				
 									<li >".$LanguageInstance->get('Pronunciation').": <b>".$partnerFeedback->pronunciation."</b></li>
 									<li >".$LanguageInstance->get('Vocabulary').": <b>".$partnerFeedback->vocabulary."</b></li>
 									<li >".$LanguageInstance->get('Grammar').": <b>".$partnerFeedback->grammar."</b></li>
@@ -123,8 +186,10 @@ if(!empty($feedBacks)){
 			$html .= "</div>";
 		}
 }
-}else
-$html .= $LanguageInstance->get('No tandems available');
+}
+if (!$show_tandem) {
+	$html .= "<br><br>".$LanguageInstance->get('No tandems available');
+}
 
 
 $html .= "</body>";
@@ -133,6 +198,7 @@ $html .= "</body>";
 
 // Print text using writeHTMLCell()
 //$pdf->writeHTMLCell(0, 0, '', '30', $html, 0, 1, 0, true, '', true);
+$pdf->SetY(20);
 $pdf->writeHTML($html,true,false,true,false,'');
 // ---------------------------------------------------------
 
