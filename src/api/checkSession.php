87b35c141b751745a898c61fde50c1b95d758c5b
Antoni Bertran <abertranb@uoc.edu>
2016-10-27 17:19:36 +0200

    Show if user connection has been accepted

--- src/api/checkSession.php
+++ src/api/checkSession.php
@@ -10,7 +10,8 @@ $user_obj = isset($_SESSION[CURRENT_USER]) ? $_SESSION[CURRENT_USER] : false;
 $force_select_room = isset($_SESSION[FORCE_SELECT_ROOM]) ? $_SESSION[FORCE_SELECT_ROOM] : false;
 $open_tool_id = isset($_SESSION[OPEN_TOOL_ID]) ? $_SESSION[OPEN_TOOL_ID] : false;
 $sent_url = isset($_REQUEST['sent_url']) ? base64_decode($_REQUEST['sent_url']) : ''; 
-$userab = isset($_REQUEST['userab']) ? $_REQUEST['userab'] : ''; 
+$userab = isset($_REQUEST['userab']) ? $_REQUEST['userab'] : '';
+$check_if_i_accepted_connection = isset($_REQUEST['check_if_i_accepted_connection']) ? $_REQUEST['check_if_i_accepted_connection']==1 : false;
 $tandem = false;
 $gestorBD = new GestorBD();
 $return = new stdclass();
@@ -23,15 +24,23 @@ if ($id>0) {
 if ($tandem) {	  	
 	
 	//lets update the session_user
-  if( !empty($user_obj->id) && !empty($force_select_room) && !empty($open_tool_id) && !empty($sent_url) )
-	 $timePassed = $gestorBD->updateSessionUser($id,$user_obj->id,$force_select_room,$open_tool_id,$sent_url);
+  if( !empty($user_obj->id) && !empty($force_select_room) && !empty($open_tool_id) && !empty($sent_url) ) {
+      $timePassed = $gestorBD->updateSessionUser($id, $user_obj->id, $force_select_room, $open_tool_id, $sent_url);
+  }
 
   	if($gestorBD->checkTandemSession($id)){
   		if (isset($_SESSION[ID_FEEDBACK]) && $_SESSION[ID_FEEDBACK]>0) {
   			$_SESSION[ID_EXTERNAL] = $gestorBD->getFeedbackExternalIdTool($id);
   		}  		
   		$return->result = "ok";
-  	}elseif($timePassed > MAX_TANDEM_WAITING){
+  	}
+    elseif ($gestorBD->myPartnerAcceptedConnectionAndMeNot($id, $user_obj->id)) {
+        $return->other_partner_connected_and_me_not = 1;
+    }
+    elseif ($check_if_i_accepted_connection && $gestorBD->meAcceptedConnectionAndMyPartnerNot($id, $user_obj->id)) {
+        $return->i_connected_and_my_partner_not = 1;
+    }
+    elseif($timePassed > MAX_TANDEM_WAITING){
   		
   		//time has reach the limit, lets send a notification to the partner to come do the tandem
   		if($gestorBD->TandemTimeOutNotificationEmail($id,$user_obj->id,$LanguageInstance,$force_select_room,$open_tool_id,$sent_url,$userab))
